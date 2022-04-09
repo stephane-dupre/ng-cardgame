3385146360549913707506d15c985b218b5620da
Ylisdra <laura.morvan.fr@gmail.com>
2022-04-09 20:08:41 +0200

    update card-detail

--- src/app/card-detail/card-detail.component.ts
+++ src/app/card-detail/card-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Card } from '../models/card.model';
+import { Item } from '../models/item.model';
 import { CardsService } from '../services/cards.service';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-card-detail',
@@ -11,15 +13,25 @@ import { CardsService } from '../services/cards.service';
 export class CardDetailComponent {
   id: string;
   card!: Card;
+  selected: keyof Card['variants'];
 
   constructor(
     private route: ActivatedRoute,
-    private cardsService: CardsService
+    private cardsService: CardsService,
+    private cartService: CartService
   ) {
     this.route.params.subscribe((params) => {
       this.id = params['id'];
     });
+
   }
+
+  putCard() {
+    this.cartService
+      .putItem(new Item(1, this.selected, this.card))
+      .subscribe((c) => console.log(c));
+  }
+
 
   ngOnInit(): void {
     this.cardsService.getOneCard(this.id).subscribe((c) => (this.card = c));
